e80ec4933fa87603b92233ad9634e13f63f54c23
ScratchOs <Computer.backup.15@gmail.com>
2018-01-13 15:44:08 +0000

    start naviagtion

--- FinanceManager/MainPage.xaml.cs
+++ FinanceManager/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,19 +13,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace FinanceManager
-{
+namespace FinanceManager {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        ObservableCollection<string> listItems = new ObservableCollection<string>();
+        int CurrentSelection = 0;
+
+        public MainPage() {
             this.InitializeComponent();
+            Pages.ItemsSource = listItems;
+
+            listItems.Add("Home");
+            listItems.Add("Card1 Statement");
+            listItems.Add("Card2 Statement");
+            listItems.Add("Food Budget");
+
+            Pages.SelectedItem = listItems[0];
+        }
+
+        private void Pages_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            foreach (object item in e.AddedItems) {
+                string selection = item as string;
+                if (selection != null) {
+                    CurrentSelection = listItems.IndexOf(selection);
+                }
+            }
+            updateMain();
+        }
+
+        private void updateMain() {
+            if(CurrentSelection <= 0) {
+                Main.Navigate(typeof(Home));
+            } else {
+                Main.Navigate(typeof(Statement));
+            }
         }
     }
 }
